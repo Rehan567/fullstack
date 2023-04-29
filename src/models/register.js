cc174776c4df379ededae2cdf1a0b89e84c42177
Md Rehan <rehanmohd042@gmail.com>
2023-04-29 15:55:59 +0530

    my project

---
+++ src/models/register.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken")
+const userSchema= new mongoose.Schema({
+    fullname:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    mobile:{
+        type:Number,
+        require:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        require:true
+    },
+    confirmpassword:{
+        type:String,
+        require:true
+    },
+    tokens:[{
+        token:{
+            type:String,
+            require:true
+        }
+    }]
+
+    
+   
+});
+
+//generating tokens
+userSchema.methods.mytoken=async function(){
+    try {
+        const token = jwt.sign({_id:this.id.toString()},process.env.SECRET_KEY)
+        this.tokens = this.tokens.concat({token:token});
+        await this.save();
+        return token;
+    } catch (error) {
+        res.send("this is my error"+error)
+        console.log("this is my error" + error)
+    }
+}
+
+
+// create Collection 
+
+const Register= new mongoose.model("Register",userSchema);
+
+module.exports = Register;
